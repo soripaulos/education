d6e4c25b3ee3c35ed1fb30b4872db557dd825145
soripaulos <157392548+soripaulos@users.noreply.github.com>
2025-05-18 02:42:18 +0300

    Simplify setup.py to rely on pyproject.toml for metadata

--- setup.py
+++ setup.py
@@ -1,19 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-with open("requirements.txt") as f:
-	install_requires = f.read().strip().split("\n")
-
-# get version from __version__ variable in education/__init__.py
-from education import __version__ as version
-
-setup(
-	name="education",
-	version=version,
-	description="Education",
-	author="Frappe Technologies Pvt. Ltd.",
-	author_email="hello@frappe.io",
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=install_requires,
-)
+if __name__ == "__main__":
+	setup()
