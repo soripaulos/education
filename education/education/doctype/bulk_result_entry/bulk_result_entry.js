7bea87a0a3cfaad10a7560ee722f49efb43e9c03
soripaulos <157392548+soripaulos@users.noreply.github.com>
2025-06-07 13:51:25 +0300

    Update bulk_result_entry.js

--- education/education/doctype/bulk_result_entry/bulk_result_entry.js
+++ education/education/doctype/bulk_result_entry/bulk_result_entry.js
@@ -1,7 +1,6 @@
 frappe.ui.form.on('Bulk Result Entry', {
     student_group: function(frm) {
         if (frm.doc.student_group) {
-            // Fetch students in the selected group
             frappe.call({
                 method: 'frappe.client.get_value',
                 args: {
@@ -11,15 +10,10 @@ frappe.ui.form.on('Bulk Result Entry', {
                 },
                 callback: function(r) {
                     if (r.message && r.message.students) {
-                        // Clear existing table
                         frm.clear_table('results');
-                        
-                        // Add each student to the table
-                        r.message.students.forEach(function(student) {
-                            var row = frm.add_child('results');
+                        r.message.students.forEach(student => {
+                            const row = frm.add_child('results');
                             row.student = student.student;
-                            
-                            // Fetch and set student name
                             frappe.model.set_value(
                                 row.doctype,
                                 row.name,
@@ -27,7 +21,6 @@ frappe.ui.form.on('Bulk Result Entry', {
                                 student.student_name
                             );
                         });
-                        
                         frm.refresh_field('results');
                     }
                 }
@@ -35,33 +28,33 @@ frappe.ui.form.on('Bulk Result Entry', {
         }
     },
     
-    before_save: function(frm) {
-        // Validate scores
-        var max_score = frm.doc.max_score;
-        frm.doc.results.forEach(function(row) {
+    validate: function(frm) {
+        // Validate scores before saving
+        const max_score = frm.doc.max_score;
+        frm.doc.results.forEach(row => {
             if (row.score > max_score) {
-                frappe.throw("Score for " + row.student_name + " exceeds max score of " + max_score);
+                frappe.throw(`Score for ${row.student_name} exceeds max score of ${max_score}`);
             }
         });
     },
     
     on_submit: function(frm) {
-        // Create individual exam result records
-        frm.doc.results.forEach(function(row) {
-            var exam_result = frappe.model.make_new_doc({
-                doctype: 'Student Exam Result',
-                student: row.student,
-                academic_term: frm.doc.academic_term,
-                subject: frm.doc.subject,
-                assessment_criteria: frm.doc.assessment_criteria,
-                score: row.score,
-                max_score: frm.doc.max_score,
-                student_group: frm.doc.student_group
-            });
-            
-            exam_result.insert();
+        // Create and submit individual exam results
+        frm.call({
+            method: 'education_tools.utils.create_exam_results_from_bulk',
+            args: {
+                bulk_entry: frm.doc.name
+            },
+            freeze: true,
+            freeze_message: __('Creating exam records...'),
+            callback: function(r) {
+                if (r.message) {
+                    frappe.msgprint({
+                        title: __('Success'),
+                        message: __('Created {0} exam records', [r.message.length])
+                    });
+                }
+            }
         });
-        
-        frappe.msgprint("Created individual exam records for all students");
     }
 });
