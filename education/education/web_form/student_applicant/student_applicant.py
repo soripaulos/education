0908b4d4d71606856e4d2f114d9ba5f8ce318bab
soripaulos <157392548+soripaulos@users.noreply.github.com>
2025-07-10 07:07:15 +0300

    Fix web form kebele undefined error by adding missing fields to Student Applicant web form

--- education/education/web_form/student_applicant/student_applicant.py
+++ education/education/web_form/student_applicant/student_applicant.py
@@ -2,5 +2,18 @@ import frappe
 
 
 def get_context(context):
-	# do your magic here
-	pass
+	# Initialize fields to prevent undefined errors
+	context.kebele = context.get('kebele', '')
+	context.sub_city = context.get('sub_city', '')
+	context.custom_school_id = context.get('custom_school_id', '')
+	context.image = context.get('image', '')
+	
+	# Set default values for common fields
+	if not context.get('nationality'):
+		context.nationality = 'Ethiopian'
+	if not context.get('city'):
+		context.city = 'Adama'
+	if not context.get('state'):
+		context.state = 'Oromia'
+	if not context.get('country'):
+		context.country = 'Ethiopia'
